5783c462b283c4cf307ad53b06eee2340cbb5f9a
nuru999 <70575815+nuru999@users.noreply.github.com>
2024-02-16 17:34:28 +0300

    updated

--- website/auth.py
+++ website/auth.py
@@ -1,33 +1,35 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login', methods=['GET', 'post'])
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", boolean=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
-
-@auth.route('/sign-up', methods=['GET', 'post'])
-def sign_up():
-   if request.method == 'POST':
-       email = request.form.get('email')
-       firstName = request.form.get('email')
-       Password1 =  request.form.get('firstName')
-       password2 =  request.form.get('password2')
-
-       if len(email) < 4:
-           flash('Email must be Greater than 4 Characters.', category= 'error')
-       elif len(firstName) < 2:
-           flash('First name must be greater than 1 Characters.', category= 'error')
-       elif Password1 != password2:
-           flash('Hey!, Password Don\'t Much, Please try Again.', category= 'error')
-       elif len(Password1) < 7:
-           flash('Password must be at least 7 characters.', category= 'error')
-       else:
-           flash('Account Created!', category= 'success')
-
-   return render_template("sign_up.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
